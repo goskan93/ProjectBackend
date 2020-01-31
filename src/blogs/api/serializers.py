c33575b9ce3974b82cad28b107a922c8887e671a
Natalia Goska <natalia.goska@agrometrika.com.br>
2020-01-31 09:30:35 -0300

    add test validation

--- src/blogs/api/serializers.py
+++ src/blogs/api/serializers.py
@@ -15,13 +15,24 @@ class CountrySerializer(serializers.ModelSerializer):
 
 
 class BlogSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        if not data['instagram'] and not data['facebook'] and not data['youtube'] and not data['website']:
+            raise serializers.ValidationError("At least one social media or website must be given.")
+        return data
+
+    ## check this later
+    def validate_countries(self, countries):
+        if countries.length <= 0:
+            raise serializers.ValidationError("At least one country must be given.")
+        return countries
+
     class Meta:
         model = Blog
         fields = ('blogId', 'name', 'website', 'youtube', 'instagram', 'facebook', 'about',
                 'flaTravelWithChildren', 'flaTravelWithAnimals', 'flaOrganizeTrips', 'countries', 'languages')
         read_only_fields = ('userId',)
-
-
+    
+    
 class UsersBlogsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
